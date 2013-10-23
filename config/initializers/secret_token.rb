4bc1f3ba208de526f3628d9ad3492556730b5d07
Christine Coulter <christinetcoulter@gmail.com>
2013-10-23 10:45:26 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BabyNames::Application.config.secret_token = '14ff5b8ba97a417858b8d60b82bf9fa5e93c42faad39e4749df9b5a08d7a2f16b2c4986fb0385bde27b255dee5431fbe50abf9b2c93127d6948f66db2db6cae0'
